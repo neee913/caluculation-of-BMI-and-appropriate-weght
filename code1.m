8abcc97593482a2792bdc102e20dfa86126e9d6b
en <114519892+neee913@users.noreply.github.com>
2022-10-06 03:15:34 +0900

    Update code1.m

--- code1.m
+++ code1.m
@@ -1,9 +1,10 @@
+%ダウンロードしたファイルの名前を変更した場合はそれに応じて（）内のファイル名を変更する
 data=readtable("code1 sample.xlsx")
 
+
+%変数を3つ追加。それぞれ適正体重の計算、適正体重と体重の比較、BMIの計算。
 data.appropriate_weight = (data.height/100).^2*22
 
 data.comparison_with_appropriate_weight = (data.appropriate_weight)-(data.weight)
 
 data.BMI = (data.weight)./(data.height/100).^2
-
-
